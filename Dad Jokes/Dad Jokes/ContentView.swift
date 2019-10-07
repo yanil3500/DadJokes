ad9d81c549e380f98de959a0393519c324047c80
Elyanil <yanil3500@gmail.com>
2019-10-07 16:57:45 -0400

    project scaffolding

---
+++ Dad Jokes/Dad Jokes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dad Jokes
+//
+//  Created by Elyanil Liranzo Castro on 10/7/19.
+//  Copyright © 2019 Elyanil Liranzo Castro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
